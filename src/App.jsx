65a5ae482a42659543d1dfa26e2631e2804e6543
diyaprajapati <diyabprajapati2005@gmail.com>
2024-05-07 11:00:07 +0530

    change bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import Footer from "./home/Foot"
 export default function App() {
   return (
     <>
-    <div className="p-4 flex flex-col gap-12 font-inter bg-slate-200">
+    <div className="p-4 flex flex-col gap-12 font-inter">
         <Header/>
         <Body/>
         <Footer/>
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 right-200">
+        <circle cx="50" cy="50" r="50" />
+        </svg>
       </div>
     </>
   )
--- src/home/Body.jsx
+++ src/home/Body.jsx
@@ -25,12 +25,6 @@ export default function Body() {
             <img src='../public/diya-main.png' width={800}></img>
         </div>
 
-        {/* circle
-        <div className='align-bottom'>
-            <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-            <circle r="45" cx="50" cy="50" fill="red" />
-            </svg>
-        </div> */}
     </div>
   )
 }
